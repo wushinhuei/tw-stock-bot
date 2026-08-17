2fa013d9f9c6eecb5df49e1409b4612a73b4b149
wushinhuei <wu.shinhuei@gmail.com>
2026-08-17 18:50:04 +0800

    Update APPS_SCRIPT_ENDPOINT URL

--- 台股策略系統/web/apps_script_config.js
+++ 台股策略系統/web/apps_script_config.js
@@ -1,1 +1,1 @@
-window.APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbypTZQZhr-B_goVM2NlQtOR6OUuCAzNhrmUtsYy-OHXIRFU5PxXfOEpzjv1igRF1FXa/exec';
+window.APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyPpK7Q2TFv-GKT_SSwMhOE1oQf8EqKlU9KzVmexunK1kNI4-CyQhoZ6YetcQtENdB8/exec';
