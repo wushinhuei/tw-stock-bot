48fc04d6a6d707a0afcc45a654f20d9b90df652b
shinhuei <wu.shinhuei@gmail.com>
2026-08-21 23:36:23 +0800

    Point dashboard to Apps Script version 42

--- 台股策略系統/web/apps_script_config.js
+++ 台股策略系統/web/apps_script_config.js
@@ -1,1 +1,1 @@
-window.APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxD-_3fXJsbaWb_8r6_tmqghUcnq-vtFbA3com3N8ViE__jEsVihkdvYNdXp90_ACKp/exec';
+window.APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyULTo0Hn09Z88rdmaw1VQVyA2xCWH0PiAz43u_3Bo47b5t5JlkDCy7uWOUjRBQXHL-/exec';
