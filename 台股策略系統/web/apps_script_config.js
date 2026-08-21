c58ab42e3dcac9402f8bbd35fad98e7dbc18e1e3
wushinhuei <wu.shinhuei@gmail.com>
2026-08-21 11:36:36 +0800

    Sync rebuilt dashboard to GitHub Pages

--- 台股策略系統/web/apps_script_config.js
+++ 台股策略系統/web/apps_script_config.js
@@ -1,1 +1,10 @@
-window.APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxD-_3fXJsbaWb_8r6_tmqghUcnq-vtFbA3com3N8ViE__jEsVihkdvYNdXp90_ACKp/exec';
+window.TW_STOCK_BOT_CONFIG = {
+  endpoint: "https://script.google.com/macros/s/AKfycbxD-_3fXJsbaWb_8r6_tmqghUcnq-vtFbA3com3N8ViE__jEsVihkdvYNdXp90_ACKp/exec",
+  refreshIntervalMs: 30 * 60 * 1000,
+  statusIntervalMs: 60 * 1000,
+  requestTimeoutMs: 75 * 1000,
+  simulationStartDate: "2026-08-20",
+  initialCapital: 100000,
+  settingsSheetUrl: "https://docs.google.com/spreadsheets/d/1TLouSeqNnj6K0xgl9-euBtLOEoj-hSHgJuZMtWQgopA/edit"
+};
+
